5db80b0c66a5c8af0c2f66f0649df39e75d9d29f
li_peng_bo_0304 <1391328647@qq.com>
2020-05-08 20:46:09 +0800

    完成了基本的home页面

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,8 +1,84 @@
 <template>
-  <div>首页</div>
+  <el-container class="home-container">
+    <!-- 头部区域 -->
+    <el-header>
+      <div>
+        <img src="../assets/logo.png" />
+        <span>电商后台管理系统</span>
+      </div>
+
+      <el-button type="info" @click="logout">退出</el-button>
+    </el-header>
+    <!-- 右侧主体 -->
+    <el-container>
+      <el-aside width="200px">
+        <!-- 侧边栏菜单区域 -->
+        <el-menu background-color=" rgb(4, 118, 163)" text-color="#fff" active-text-color="#ffd04b">
+          <!-- 一级菜单 -->
+          <el-submenu index="1">
+            <!-- 一级菜单的模板区域 -->
+            <template slot="title">
+              <!-- 图标 -->
+              <i class="el-icon-location"></i>
+              <!-- 文本 -->
+              <span>导航一</span>
+            </template>
+            <!-- 二级菜单 -->
+            <el-menu-item index="1-4-1">
+              <template slot="title">
+                <i class="el-icon-location"></i>
+                <span>导航一</span>
+              </template>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <!-- 页面主体 -->
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
+
 <script>
-export default {}
+export default {
+  methods: {
+    logout() {
+      window.sessionStorage.clear()
+      this.$router.push('/login')
+    }
+  }
+}
 </script>
+
 <style lang="less" scoped>
+.el-header {
+  background-color: rgb(7, 111, 247);
+  display: flex;
+  justify-content: space-between;
+  padding-left: 0;
+  align-items: center;
+  color: #fff;
+  font-size: 20px;
+  > div {
+    display: flex;
+    align-items: center;
+    span {
+      margin-left: 15px;
+    }
+  }
+  img {
+    margin-left: 10px;
+    width: 50px;
+    height: 50px;
+  }
+}
+.el-aside {
+  background-color: rgb(4, 118, 163);
+}
+.el-main {
+  background-color: rgb(203, 224, 241);
+}
+.home-container {
+  height: 100%;
+}
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '../components/Login.vue'
 import Home from '../components/Home.vue'
-import { setTitle, setToken, getToken } from '@/lib/util'
+
 Vue.use(Router)
 
 const router = new Router({
@@ -18,36 +18,6 @@ const router = new Router({
       path: '/home', component: Home
     }
   ]
-})
-
-// 全局前置守卫
-/*
-to: Route: 即将要进入的目标 路由对象
-from: Route: 当前导航正要离开的路由
-next: Function: 一定要调用该方法来 resolve 这个钩子
- */
-// 模拟登陆验证逻辑:当跳转页面为登陆页面且已经登陆时，直接跳转到home页面，如果跳转页面不为登录页且已经登陆，则继续执行，否则直接跳转到登录页
-router.beforeEach((to, from, next) => {
-  to.meta && setTitle(to.meta.title)
-  debugger
-  const token = getToken()
-  if (token) {
-      .dispatch('authorization', token)
-      .then(() => {
-        if (to.name === 'login') {
-          next({ name: 'home' })
-        } else {
-          next()
-        }
-      })
-      .catch(() => {
-        setToken('')
-        next({ name: 'login' })
-      })
-  } else {
-    if (to.name === 'login') next()
-    else next({ name: 'login' })
-  }
 })
 
 export default router
